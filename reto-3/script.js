d3c9b349268e66ce1f743d08b73a0b718e2bb796
AngelicaDiavanera <mdiavanera@gmail.com>
2022-03-24 21:27:39 -0500

    Ejercicios-logica-js-3

---
+++ reto-3/script.js
@@ -0,0 +1,32 @@
+function obtenerMesDelAño() {
+  for (mes of obtenerMesDelAño) {
+    if (mes == "1") {
+      return "Enero";
+    } else if (mes == "2") {
+      return "Febrero";
+    } else if (mes == "3") {
+      return "Marzo";
+    } else if (mes == "4") {
+      return "Abril";
+    } else if (mes == "5") {
+      return "Mayo";
+    } else if (mes == "6") {
+      return "junio";
+    } else if (mes == "7") {
+      return "Julio";
+    } else if (mes == "8") {
+      return "Agosto";
+    } else if (mes == "9") {
+      return "Septiembre";
+    } else if (mes == "10") {
+      return "Octubre";
+    } else if (mes == "11") {
+      return "Noviembre";
+    } else if (mes == "12") {
+      return "diciembre";
+    } else {
+      return "no soy nungun mes";
+    }
+  }
+}
+console.log(mes);
